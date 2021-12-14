6a0e69307acc4d5636b15a9187d314fe5d015e56
haru5386 <snsn550@gmail.com>
2021-12-14 22:25:38 +0800

    feat: project init

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Navbar/>
     <router-view />
   </div>
 </template>
+<script>
 
+import '@/assets/scss/main.scss'
+
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Navbar,
+  },
+};
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
