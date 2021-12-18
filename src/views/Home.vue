ea7be82517654d24c8ef099808fabd9022546126
haru5386 <snsn550@gmail.com>
2021-12-18 15:05:01 +0800

    feat: nav bar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="home">
-    <Navbar/>
   </div>
 </template>
 
 <script>
-import Navbar from "@/components/Navbar.vue";
+
 
 export default {
   name: "Home",
   components: {
-    Navbar,
   },
 };
 </script>
